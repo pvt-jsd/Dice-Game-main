ea9193ae08abcf125acdedf088d9434b04c911d0
pvt-jsd <pvt.jsd@gmail.com>
2023-03-25 12:45:04 +0530

    done

---
+++ logic.js
@@ -0,0 +1,119 @@
+'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+let diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const player0current = document.querySelector('#current--0');
+const player1current = document.querySelector('#current--1');
+
+
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+let playing = true;
+
+
+function switchPlayer() {
+
+    
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+
+};
+// Rolling Dice
+
+btnRoll.addEventListener('click', function () {
+
+    if (playing) {
+
+       
+        const dice = Math.trunc(Math.random() * 6) + 1;
+
+
+        // 2. Display Dice
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`;
+        
+        
+        
+        // 3.Check if its 1
+        if (dice !== 1) {
+
+            //Add dice to current score
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        }
+        else {
+
+
+            //Switch to next player
+
+            switchPlayer();
+
+        }
+    }
+
+})
+
+
+btnHold.addEventListener('click', function () {
+
+    if (playing) {
+        scores[activePlayer] += currentScore;
+
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+        if (scores[activePlayer] >= 100) {
+            playing = false;
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+            diceEl.classList.add('hidden');
+
+        }
+        else {
+
+            switchPlayer();
+        }
+    }
+
+})
+
+
+
+btnNew.addEventListener('click', function () {
+    
+    currentScore = 0;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    diceEl.classList.add('hidden');
+    scores = [0, 0];
+    activePlayer = 0;
+    playing = true;
+    player1current.textContent=0;
+    player0current.textContent=0;
+
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player1El.classList.remove('player--active');
+    player0El.classList.add('player--active');
+
+    scores[activePlayer] = 0;
+
+
+})
